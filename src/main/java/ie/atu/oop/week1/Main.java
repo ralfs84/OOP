b0f9071d7468feffdc67763efc721c6d0c297075
Ralfs <g00483357@atu.ie>
2026-09-23 16:39:25 +0100

    Created second instance of Book class and this object is called secondbook

--- src/main/java/ie/atu/oop/week1/Main.java
+++ src/main/java/ie/atu/oop/week1/Main.java
@@ -13,8 +13,21 @@ public class Main {
         firstbook.pageCount = 4;
 
         System.out.println("This book is " + firstbook.title);
-        System.out.println("This book is writen by  " + firstbook.author);
+        System.out.println("This book is written by  " + firstbook.author);
         System.out.println("pages: " + firstbook.pageCount);
         System.out.println("available: " + firstbook.available);
+        System.out.println("\n");
+
+        Book secondbook = new Book();
+        secondbook.title = "Bune 2";
+        secondbook.author = "Ralfs Vilsars";
+        secondbook.pageCount = 21;
+        secondbook.available = true;
+
+        System.out.println("This book is " + secondbook.title);
+        System.out.println("This book is written by  " + secondbook.author);
+        System.out.println("pages: " + secondbook.pageCount);
+        System.out.println("available: " + secondbook.available);
+        System.out.println("\n");
     }
 }
