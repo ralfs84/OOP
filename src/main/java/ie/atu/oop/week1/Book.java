5eb0e08eaaa444002619a2bf967bd0083e8e7100
Ralfs <g00483357@atu.ie>
2026-09-23 16:29:17 +0100

    Created a new book class and created an instance in Main

---
+++ src/main/java/ie/atu/oop/week1/Book.java
@@ -0,0 +1,8 @@
+package ie.atu.oop.week1;
+
+public class Book {
+    public String title;
+    public String author;
+    public int pageCount;
+    public boolean available = true;
+}
--- src/main/java/ie/atu/oop/week1/Main.java
+++ src/main/java/ie/atu/oop/week1/Main.java
@@ -6,5 +6,15 @@ public class Main {
     public static void main(String[] args)
     {
         System.out.println("hello");
+
+        Book firstbook = new Book();
+        firstbook.title = "Bune";
+        firstbook.author = "Brank Berbert";
+        firstbook.pageCount = 4;
+
+        System.out.println("This book is " + firstbook.title);
+        System.out.println("This book is writen by  " + firstbook.author);
+        System.out.println("pages: " + firstbook.pageCount);
+        System.out.println("available: " + firstbook.available);
     }
 }
